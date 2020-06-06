10fae1b5737c7ab5656e3ab6c740800f8443f6b3
SonyaShtern <55851932+SonyaShtern@users.noreply.github.com>
2020-06-06 09:46:51 +0300

    Delete adding_word.py

--- adding_word.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def adding(message):
-    if re.search(r'[a-zA-Z]*\;[а-яА-Я]', message):
-        with open("dictionary.csv", "a", encoding = "utf-8") as file:
-            file.write(message + "\n")
-            return "Вы прекрасны! \nСпасибо!"
-    else:
-        return "Неверный формат. \nПерепроверьте и попробуйте снова."
